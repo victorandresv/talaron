6390f2dcbf1b437df1522ed6dbd5ae9819c25903
Victor Vargas <victorvargas@192.168.0.102>
2022-11-23 11:17:33 -0300

    fixing install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 npm install --global yarn
+
 yarn add jsonwebtoken@8.5.1
+yarn add crypto@1.0.1
+yarn add dotenv@16.0.0
+yarn add express@4.17.3
+yarn add redis@4.0.4
+
 node /app/server.js
